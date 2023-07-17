1c6676295fef566447f61fd174ede689f19ba8bb
wish13t <vishal13joshi@gmail.com>
2023-07-17 23:47:13 +0530

    Worked till reset password

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,4 @@
 class MainController < ApplicationController
     def index
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end
-        # flash.now[:notice] = "You opened page succesfully"
-        # flash.now[:alert] = "Your page opening failed!"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
@@ -10,6 +13,13 @@ Rails.application.routes.draw do
   post "sign_in", to: "sessions#create"
 
   delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+  
+
   root to: "main#index"
   # Defines the root path route ("/")
   # root "articles#index"
